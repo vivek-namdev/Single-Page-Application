b144585a79a69607b0529bba075125a21a715a70
Vivek Namdev <viveknamdev257@gmail.com>
2024-11-01 15:18:04 +0530

    added layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,86 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 
 function App() {
-  
-  return (
-    <div>
-      Allen | Class 11 | Class 12
-      <BrowserRouter>
-      <Routes>
-        <Route path="/neet/online-coaching-class-11" element={<Class11Program/>}/>
-        <Route path="/neet/online-coaching-class-12" element={<Class12Program/>}/>
-        <Route path="/" element={<Landing/>}/>
-      </Routes>
-      </BrowserRouter>
-    </div>
-  )
+
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route path="/" element={<Landing />} />
+                        <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
+                        <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
+                        <Route path="*" element={<ErrorPage />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+
+function ErrorPage() {
+
+    return (
+        <div>
+            <h1>404 Page not found</h1>
+        </div>
+    );
+}
+
+
+function Layout() {
+
+    return (
+        <div style={{ height: "100vh" }}>
+            
+            <nav style={{ background: "yellow" }}>
+                <Link to="/">Allen</Link> |
+                <Link to="/neet/online-coaching-class-11">Class 11</Link> |
+                <Link to="/neet/online-coaching-class-12">Class 12</Link>
+            </nav>
+            
+            <div style={{ height: "80vh", background: "gray" }}>
+                <Outlet />
+            </div>
+            
+            <footer style={{ background: "yellow" }}>Footer | Contact Us</footer>
+        </div>
+    );
 }
 
 function Landing() {
-  return (
-    <div>
-      Welcome to allen
-    </div>
-  )
+    return (
+        <div>
+            <h1>Welcome to Allen</h1>
+        </div>
+    );
 }
 
 function Class11Program() {
-  return (
-    <div>
-      Neet Program For Class 11
-    </div>
-  )
+
+    return (
+        <div>
+            <h2>Neet Programs for Class 11th</h2>
+        </div>
+    );
 }
 
 function Class12Program() {
-  return (
-    <div>
-      Neet Program For Class 12
-    </div>
-  )
+
+    const navigate = useNavigate();
+
+    function redirectUser() {
+        navigate("/");
+    }
+
+    return (
+        <div>
+            <h2>Neet Programs for Class 12th</h2>
+
+            <button onClick={redirectUser}>Back to Landing Page</button>
+        </div>
+    );
 }
 
-export default App
+export default App;
